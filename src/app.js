1444aded69294c8a5478408c28d478148c05d046
kayoubi <khaled.ayoubi@gmail.com>
2017-01-07 10:45:43 -0800

    Basic firebase config

--- src/app.js
+++ src/app.js
@@ -3,8 +3,21 @@ import { Text } from 'react-native'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import reducers from './reducers'
+import firebase from 'firebase'
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp(
+      {
+        apiKey: "AIzaSyBFuu2RotqE8jUJTkC-1xEUBVvD-uDAoQ0",
+        authDomain: "react-native-auth-8f797.firebaseapp.com",
+        databaseURL: "https://react-native-auth-8f797.firebaseio.com",
+        storageBucket: "react-native-auth-8f797.appspot.com",
+        messagingSenderId: "36348196574"
+      }
+    );
+  }
+
   render() {
     return (
       <Provider store={createStore(reducers)}>
